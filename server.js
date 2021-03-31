072d0e9359ab3137a28166e90df5e9b69554a954
James Prickitt <jwprickitt@gmail.com>
2021-03-31 11:36:16 -0400

    first working connection and submit new todo

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'todo'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} database`)
+        db = client.db(dbName)
+    })
+    .catch(error => console.log(error))
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    db.collection('todojs').find().sort({priority:-1}).toArray()
+        .then(data => {
+        res.render('index.ejs',{info:data})
+        })
+    .catch(error => console.error(error))
+})
+
+app.post('/addTodo', (req, res) => {
+    db.collection('todojs').insertOne({ description: req.body.description, priority: req.body.priority, done: false })
+        .then(result => {
+            console.log('Todo Added')
+            res.redirect('/')
+        })
+        .catch(error => console.error(error))
+})
+    
+
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
